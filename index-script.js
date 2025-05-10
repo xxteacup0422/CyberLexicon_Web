1e4f79ddf064155ac82a0165bcfe6346591454dd
unknown <xxtcang@gmail.com>
2025-05-10 18:41:28 +0800

    2025.5.10 18:41

--- index-script.js
+++ index-script.js
@@ -72,10 +72,5 @@ function showInfo() {
 }
 
 function backMain() {
-<<<<<<< HEAD
-    window.location.href.substr(0, window.location.href.indexOf('#'))
+    window.location.href = "index.html"
 }
-=======
-    window.location.href = "/";
-}
->>>>>>> 80ad283c7c81015fa827a91eddd53a9080683941
